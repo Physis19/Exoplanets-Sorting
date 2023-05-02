08e5093eb0b9c7cdb7c0b16798af90df4fe76d3a
Kauã Fellipe <117491674+Physis19@users.noreply.github.com>
2023-05-02 18:45:10 -0300

    Delete extracted data.py

--- extracted data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-dados = pd.read_csv('exoplanets_new.csv', usecols=[6])
-array_dados = dados.values
-exoplanets_years = pd.DataFrame(array_dados, columns=['Years'])
-exoplanets_years.to_csv('exoplanets_years.csv')
